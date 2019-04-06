3d9596798c365e7c48ce52e4bdf9475b296f5029
adit <aeka489@gmail.com>
2019-04-07 04:16:39 +0700

    Create Adapter

--- app/src/main/java/com/adityaeka/sampahisasi/adapter/ChapterAdapter.java
+++ app/src/main/java/com/adityaeka/sampahisasi/adapter/ChapterAdapter.java
@@ -1,4 +1,30 @@
 package com.adityaeka.sampahisasi.adapter;
 
-public class ChapterAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChapterAdapter extends RecyclerView.Adapter<ChapterAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
