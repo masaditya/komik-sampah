5d2655867e2845681666d67b8123f968d5b25ead
adit <aeka489@gmail.com>
2019-04-10 12:11:42 +0700

    change UI again

--- app/src/main/java/com/adityaeka/sampahisasi/ListComicActivity.java
+++ app/src/main/java/com/adityaeka/sampahisasi/ListComicActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -140,6 +142,15 @@ public class ListComicActivity extends AppCompatActivity {
                 intentRead.putExtra("idReadComic", idReadComic);
                 startActivity(intentRead);
 
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(ListComicActivity.this, AddComicActivity.class);
+                startActivity(in);
             }
         });
 
